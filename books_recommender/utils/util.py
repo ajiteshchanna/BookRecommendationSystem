bd1dfae8817f516a3ab8a1fc49eebd96e70c86eb
Ajitesh Channa <ac.ajiteshchanna@gmail.com>
2026-04-01 11:44:33 +0530

    updating util.py

--- books_recommender/utils/util.py
+++ books_recommender/utils/util.py
@@ -9,6 +9,5 @@ def read_yaml_file(file_path:str)->dict:
     try:
         with open(file_path,'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
-        
     except Exception as e:
         raise AppException(e,sys) from e
